942dd7f141ba683fc7a4f48c1467ee335f94bbdf
Hao Chen <haoc@utexas.edu>
2020-07-31 18:15:07 -0500

    init something

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from util.config import params_setup, logging_setup
+from netlist import netlist
+
+def main():
+    para = params_setup()
+    
+if __name__ == "__main__":
+    main()
