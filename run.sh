03aec04ae372466812ca00626d37284f6d173d2b
Keren Zhu <keren@cerc.utexas.edu>
2020-09-19 14:08:57 -0500

    add adc1

--- run.sh
+++ run.sh
@@ -5,8 +5,10 @@ BENCH=./benchmark
 python3 main.py --netlist $BENCH/CTDSM_CORE_NEW.sp \
                           $BENCH/ADC_CORE.sp \
                           $BENCH/CTDTDSM_V3.sp \
+                          $BENCH/adc1.sp \
                 --sym $BENCH/sym/CTDSM_CORE_NEW.sym \
                       $BENCH/sym/ADC_CORE.sym \
-                      $BENCH/sym/CTDTDSM_V3.sym
+                      $BENCH/sym/CTDTDSM_V3.sym \
+                      $BENCH/sym/adc1.sym
 
 
